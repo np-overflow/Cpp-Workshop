ee44ed490beac10f72df6cb249b6384b1c45d9e7
Felix Dimjati <100569717+felixcodesstuff@users.noreply.github.com>
2025-10-24 23:07:44 +0800

    tom paid me to fix it

--- toms_shapes.cpp
+++ toms_shapes.cpp
@@ -1,29 +1,19 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 void printStuff(int x, int y) {
-	for (int count = {1}; count <= y; ++count) {
-		for (int i = 0; i <= x; ++i) {
-			std::cout << "*";
-		}
-		std::cout << "\n";
-	}
-
+    for (int countRow = 0; countRow < y; ++countRow) { //to run the loop for each row until all rows done
+        for (int countCol = 0; countCol < x; ++countCol) { //to run the loop for each * until all in a row done
+            std::cout << "*";
+        }
+        std::cout << std::endl;   // flush each row
+    }
 }
 
-
-int main()
-{
-	int width, height;
-	std::cout << "Enter width and height with a space in between: ";
-	std::cin >> width >> height;
-	printStuff(width, height);
+int main() {
+    int width, height;
+    std::cout << "Enter width and height with a space in between: " << std::endl;
+    std::cin >> width >> height;
+    printStuff(width, height);
     return 0;
 }
+
